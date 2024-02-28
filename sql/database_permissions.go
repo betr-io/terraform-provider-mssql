926e9a8c8fcb8a36eeceeb2b4affaedb9719d4fd
ValeruS <zetdot@hotmail.com>
2024-02-28 13:33:01 +0200

    database permission update

--- sql/database_permissions.go
+++ sql/database_permissions.go
@@ -11,7 +11,7 @@ func (c *Connector) GetDatabasePermissions(ctx context.Context, database string,
   cmd := `DECLARE @stmt nvarchar(max)
           SET @stmt = 'SELECT DISTINCT pr.principal_id, pr.name, ' +
                       'pe.permission_name ' +
-                      'FROM sys.database_principals AS pr LEFT JOIN sys.database_permissions AS pe ' +
+                      'FROM [sys].[database_principals] AS pr LEFT JOIN [sys].[database_permissions] AS pe ' +
                       'ON pe.grantee_principal_id = pr.principal_id ' +
                       'WHERE pr.name = ' + QuoteName(@username, '''')
           EXEC (@stmt)`
@@ -81,6 +81,43 @@ func (c *Connector) CreateDatabasePermissions(ctx context.Context, permissions *
     setDatabase(&permissions.DatabaseName).
     ExecContext(ctx, cmd,
       // sql.Named("database_name", permissions.DatabaseName),
+      sql.Named("username", permissions.UserName),
+      sql.Named("permissions", strings.Join(permissions.Permissions, ",")),
+    )
+}
+
+func (c *Connector) UpdateDatabasePermissions(ctx context.Context, permissions *model.DatabasePermissions) error {
+  cmd := `DECLARE @stmt nvarchar(max)
+          DECLARE grant_perm_cur CURSOR FOR SELECT value FROM String_Split(@permissions, ',') WHERE value NOT IN(SELECT permission_name FROM [sys].[database_permissions] pe, [sys].[database_principals] pr WHERE pe.grantee_principal_id = pr.principal_id AND pr.name = @username)
+          DECLARE revoke_perm_cur CURSOR FOR SELECT pe.permission_name FROM [sys].[database_principals] pr LEFT JOIN [sys].[database_permissions] pe ON pe.grantee_principal_id = pr.principal_id AND pr.name = @username AND pe.permission_name != 'CONNECT' AND pe.permission_name NOT IN (SELECT value FROM String_Split(@permissions, ','))
+          DECLARE @perm_name nvarchar(max)
+
+          OPEN grant_perm_cur
+          FETCH NEXT FROM grant_perm_cur INTO @perm_name
+          WHILE @@FETCH_STATUS = 0
+            BEGIN
+              SET @stmt = 'GRANT ' + @perm_name + ' TO ' + QuoteName(@username)
+              EXEC (@stmt)
+              FETCH NEXT FROM grant_perm_cur INTO @perm_name
+            END
+          CLOSE grant_perm_cur
+          DEALLOCATE grant_perm_cur
+
+          OPEN revoke_perm_cur
+          FETCH NEXT FROM revoke_perm_cur INTO @perm_name
+          WHILE @@FETCH_STATUS = 0
+            BEGIN
+              SET @stmt = 'REVOKE ' + @perm_name + ' FROM ' + QuoteName(@username)
+              EXEC (@stmt)
+              FETCH NEXT FROM revoke_perm_cur INTO @perm_name
+            END
+          CLOSE revoke_perm_cur
+          DEALLOCATE revoke_perm_cur
+          `
+  return c.
+  setDatabase(&permissions.DatabaseName).
+    ExecContext(ctx, cmd,
+      // sql.Named("database", database),
       sql.Named("username", permissions.UserName),
       sql.Named("permissions", strings.Join(permissions.Permissions, ",")),
     )
